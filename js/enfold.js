2d5415139e9bb1d8b8e62a5e27d6125bd725ac6c
booldook <booldook@gmail.com>
2020-05-18 13:14:34 +0900

    enfold header 완성

--- js/enfold.js
+++ js/enfold.js
@@ -2,6 +2,8 @@
 // $(".navi-wrap").childrend(".navi")
 $(".navi-wrap").find(".navi").hover(function(){
 	$(this).find(".subs").stop().fadeIn(300);
+	$(this).find(".bar").addClass("active");
 }, function(){
 	$(this).find(".subs").stop().fadeOut(300);
+	$(this).find(".bar").removeClass("active");
 });
